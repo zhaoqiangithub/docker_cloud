500bf02ebc43ebef28526afd022de86d192fc491
zhaoqiang <17691274108@163.com>
2019-04-14 13:53:42 +0800

    docker_cloud打包项目插件第一次提交

---
+++ src/main/java/com/zq/docker_cloud/DockerCloudApplication.java
@@ -0,0 +1,15 @@
+package com.zq.docker_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DockerCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerCloudApplication.class, args);
+    }
+
+}
