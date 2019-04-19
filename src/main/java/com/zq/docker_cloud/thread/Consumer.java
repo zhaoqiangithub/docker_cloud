9a4b18a14da7ac0a252ad2c75baa1b25d391ed71
zhaoqiang <17691274108@163.com>
2019-04-20 01:14:13 +0800

    新增基于普通Thread/Runable 模拟的生产着消费者模型 新增基于Callable 模拟的小明做菜模型

---
+++ src/main/java/com/zq/docker_cloud/thread/Consumer.java
@@ -0,0 +1,23 @@
+package com.zq.docker_cloud.thread;
+
+public class Consumer implements Runnable {
+
+    private Box box;
+
+    public Consumer(Box box){
+        this.box = box;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            box.consume();
+        }
+
+    }
+}
---
+++ src/main/java/com/zq/docker_cloud/thread2/Utensil.java
@@ -0,0 +1,20 @@
+package com.zq.docker_cloud.thread2;
+
+/**
+ * @ClassName Utensil
+ * @Description TODO
+ * @@Author qiang_obs
+ * @Date 2019/4/20 0:47
+ * @Version 1.0
+ **/
+public class Utensil {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
